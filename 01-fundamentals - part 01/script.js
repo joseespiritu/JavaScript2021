e26f9ab28933b3c45faf4e1a92db42c081db2d63
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-09-12 18:12:47 -0500

    JS - Fundamentals part 1

---
+++ 01-fundamentals - part 01/script.js
@@ -0,0 +1,60 @@
+/*
+let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+*/
+
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); */
+
+
+// STATEMENTS AND EXPRESSIONS
+
+// THE CONDITIONAL (TERNARY) OPERATOR
+/* const age = 23;
+age >= 18 ? console.log(`I like to drink wine 🍷`) : console.log(`I like to drink water 💧`);
+
+const drink = age >= 18 ? `wine 🍷` : `water 💧`;
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = `wine 🍷`;
+} else {
+    drink2 = `water 💧`;
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? `wine 🍷` : `water 💧`}`); */
+
+// Coding challenge #4
+/* const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? (bill * 0.15) : (bill * 0.20);
+const total = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`); */
