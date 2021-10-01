065bd8e37c20fab14c16feb581f63f58bd225eba
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-01 07:38:49 -0500

    Implementation the logic of the game

--- 04-guees-my-number/script.js
+++ 04-guees-my-number/script.js
@@ -11,9 +11,10 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
 //EVENTS
-const x = function() {
-    console.log(23);
-}
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -21,5 +22,25 @@ document.querySelector('.check').addEventListener('click', function() {
 
     if(!guess) {
         document.querySelector('.message').textContent = '⛔ No number';
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } else if (guess > secretNumber) {
+        if(score > 1 ){
+            document.querySelector('.message').textContent = `↗ Too High`;
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = `🤯 You lost the game!`;
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if(score > 1 ){
+        document.querySelector('.message').textContent = `↙ Too Low`;
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = `🤯 You lost the game!`;
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
