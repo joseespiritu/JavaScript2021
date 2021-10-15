5aa23dd5e8efd613b89393ba465a5d2f5b1e8a85
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-14 23:14:11 -0500

    Nullish Operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -45,33 +45,42 @@ const restaurant = {
   }
 };
 
-console.log('------ OR --------');
-// Use ANY data type, return ANY data type, short-circuiting
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
-
 restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+const guests = restaurant.numGuests || 10;
+console.log(guests);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
-console.log('------- AND --------');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
+// Short Cicuiting 
+// console.log('------ OR --------');
+// // Use ANY data type, return ANY data type, short-circuiting
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
 
-console.log('Hello' && 23 && null && 'Jonas');
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('Mushrooms', 'Spinach');
-}
+// restaurant.numGuests = 0;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log('------- AND --------');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// console.log('Hello' && 23 && null && 'Jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('Mushrooms', 'Spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 
 // // destructuring
