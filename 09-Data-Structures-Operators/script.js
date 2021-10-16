de857d10eb50060ef226e5421dfa48f41dec0ed9
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-16 18:43:35 -0500

    For-of loop

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -87,37 +87,47 @@ const game = {
   }
 };
 
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+for (const item of menu) console.log(item);
 
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-
-// 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-
-// 4.
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
-
-// 5.
-const { odds: { team1, x: draw, team2 } } = game;
-console.log(team1, draw, team2);
-
-// 6.
-const printGoals = function(...players) {
-  console.log(players);
-  console.log(`${players.length} goals were scored`);
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
 }
 
-printGoals(...game.scored);
+// console.log([...menu.entries()]);
 
-// 7.
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 1 is more likely to win');
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// // 3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// // 4.
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+// // 5.
+// const { odds: { team1, x: draw, team2 } } = game;
+// console.log(team1, draw, team2);
+
+// // 6.
+// const printGoals = function(...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// }
+
+// printGoals(...game.scored);
+
+// // 7.
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 1 is more likely to win');
 
 // restaurant.numGuests = 0;
 // const guests = restaurant.numGuests || 10;
