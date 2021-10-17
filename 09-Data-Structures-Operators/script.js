fa86e32967d53bf87be286fa79495869f7fa24a6
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-17 00:15:24 -0500

    Optional Chaining

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,6 +50,36 @@ const restaurant = {
   }
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon) console.log (restaurant.openingHours.mon.open);
+
+// With optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for ( const day of days ) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exists');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exists');
+
+// Arrays
+const users = [
+  {name: 'Jonas', email: 'hello@jonas.com'}
+];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
+
+
 
 //for-of loop
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
