aa308ae0ba7ad580cde180ed162c7c453d064c6c
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-21 19:39:28 -0500

    Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,21 +2,24 @@
 
 const bookings = [];
 
-const createBooking = function(flightNum, numPassengers = 1, price = 199 + numPassengers){
-    // ES5
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 + numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
 
-    const booking = {
-        flightNum,
-        numPassengers,
-        price
-    }
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
 
-    bookings.push(booking);
-    console.log(booking);
-
-}
+  bookings.push(booking);
+  console.log(booking);
+};
 
 createBooking('LH123');
 createBooking('LH123', 2, 800);
