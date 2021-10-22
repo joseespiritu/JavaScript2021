7dd7a83ba8e40618577f6eada5d1d483f0c1dc94
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-21 20:39:45 -0500

    Functions returning functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,31 +1,50 @@
 'use strict';
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  }
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original String: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
+greet('Hello')('Jonas');
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+// With arrow functions
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+greetArr('Hi')('Jose');
 
-// JS uses callbacks all the time
-const high5 = function () {
-  console.log('😁');
-};
-document.body.addEventListener('click', high5);
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+/////////////////////////////////////
+// Callback functions
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original String: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// // JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('😁');
+// };
+// document.body.addEventListener('click', high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
 
 // const flight = 'LH123';
