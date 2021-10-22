0fa8e6a825a1e8af7ee9ab5b113f3c20278b45b8
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-21 20:27:55 -0500

    Callback functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,37 +1,65 @@
 'use strict';
 
-const flight = 'LH123';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 232311212,
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 232311212) {
-    alert('Checked In');
-  } else {
-    alert('Wrong passport');
-  }
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
 };
 
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('😁');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
+// const flight = 'LH123';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 232311212,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 232311212) {
+//     alert('Checked In');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // flightNum = flight;
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
-
-// flightNum = flight;
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
-};
-
-newPassport(jonas);
-checkIn(flight, jonas);
 
 // const bookings = [];
 
