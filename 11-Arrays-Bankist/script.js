afb9908b0b939198722154036521dc166071c922
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-29 07:54:31 -0500

    Filter method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -99,7 +99,7 @@ console.log(accounts);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -230,3 +230,18 @@ console.log(accounts);
 // );
 
 // console.log(movementsDescriptions);
+
+/////////////////////////////////////
+// The filter method
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
