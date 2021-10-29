37cd8568707af2d8666a34d1512b78c2b411d56a
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-28 20:00:19 -0500

    Computing usernames

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -82,6 +82,19 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts); // stw
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -191,29 +204,29 @@ displayMovements(account1.movements);
 
 /////////////////////////
 // The Map Method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const eurToUsd = 1.1;
+// const eurToUsd = 1.1;
 
-const movementsUSD = movements.map(function (mov) {
-  return mov * eurToUsd;
-});
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
 
-const movementUSDArr = movements.map(mov => mov * eurToUsd);
-console.log(movementUSDArr);
+// const movementUSDArr = movements.map(mov => mov * eurToUsd);
+// console.log(movementUSDArr);
 
-console.log(movements);
-console.log(movementsUSD);
+// console.log(movements);
+// console.log(movementsUSD);
 
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
 
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1} You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
-      mov
-    )}`
-);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1} You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
 
-console.log(movementsDescriptions);
+// console.log(movementsDescriptions);
