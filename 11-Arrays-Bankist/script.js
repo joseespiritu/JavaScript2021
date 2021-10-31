8b0bf9ddfe801616064320773c447b9467382e76
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-10-31 08:41:21 -0600

    Chaning methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -72,7 +72,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov} €</div>
     </div>
     `;
 
@@ -87,6 +87,29 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance} EUR`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const income = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${income}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -253,7 +276,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 //////////////////////////////////////
 // The reduce method
-console.log(movements);
+// console.log(movements);
 
 // accummulator -> SNOWBALL
 // const balance = movements.reduce(function (acc, cur, i, arr) {
@@ -282,24 +305,39 @@ console.log(movements);
 // Test Data 1: [5, 2, 4, 1, 15, 8, 3];
 // Test Data 2: [16, 6, 10, 5, 6, 1, 4];
 
-const calcAverageHumanAge = function (dogAges) {
-  const humanAges = dogAges.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+// const calcAverageHumanAge = function (dogAges) {
+//   const humanAges = dogAges.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
 
-  const adults = humanAges.filter(age => age >= 18);
+//   const adults = humanAges.filter(age => age >= 18);
 
-  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 
-  const average = adults.reduce(
-    (acc, age, i, arr) => acc + age / arr.length,
-    0
-  );
+//   const average = adults.reduce(
+//     (acc, age, i, arr) => acc + age / arr.length,
+//     0
+//   );
 
-  // 2 3. (2+3)/2 = 2.5. 2/2+3/2 = 2.5
+//   // 2 3. (2+3)/2 = 2.5. 2/2+3/2 = 2.5
 
-  return average;
-};
+//   return average;
+// };
 
-const avg2 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-const avg1 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// const avg2 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg1 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
-console.log(avg1, avg2);
+// console.log(avg1, avg2);
+
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositUSD);
