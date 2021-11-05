cc43cc30e2b29e12d8460a7119010dc350a3b628
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-11-04 21:45:57 -0600

    Coding challenge #4

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -593,56 +593,118 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // Array Methods Practice
 
 // 1.
-const bankDepositsSum = accounts
-  .flatMap(acc => acc.movements)
-  .filter(mov => mov > 0)
-  .reduce((sum, cur) => sum + cur, 0);
-console.log(bankDepositsSum);
+// const bankDepositsSum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(bankDepositsSum);
 
 // 2.
 // const numDeposits1000 = accounts
 //   .flatMap(acc => acc.movements)
 //   .filter(mov => mov >= 1000).length;
 
-const numDeposits1000 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
-console.log(numDeposits1000);
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+// console.log(numDeposits1000);
 
 // Prefixed ++ opertator
-let a = 10;
-console.log(a++);
-console.log(a);
+// let a = 10;
+// console.log(a++);
+// console.log(a);
 
 // 3.
-const { deposits, withdrawals } = accounts
-  .flatMap(acc => acc.movements)
-  .reduce(
-    (sums, cur) => {
-      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
-      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
-      return sums;
-    },
-    { deposits: 0, withdrawals: 0 }
-  );
+// const { deposits, withdrawals } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
 
-console.log(deposits, withdrawals);
+// console.log(deposits, withdrawals);
 
 // 4.
 // this is a nice title => This Is a Nice Title
-const convertTitleCase = function (title) {
-  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+// const convertTitleCase = function (title) {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
 
-  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+//   const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
 
-  const titleCase = title
-    .toLowerCase()
-    .split(' ')
-    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
-    .join(' ');
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(' ');
 
-  return capitalize(titleCase);
-};
-console.log(convertTitleCase('this is a nice title'));
-console.log(convertTitleCase('this is a LONG title but not too long'));
-console.log(convertTitleCase('and here is another title with an Example'));
+//   return capitalize(titleCase);
+// };
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title but not too long'));
+// console.log(convertTitleCase('and here is another title with an Example'));
+
+/////////////////////////
+// Coding challenge #4
+// TEST DATA
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+dogs.forEach(
+  dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recommendedFood ? 'much' : 'little'
+  }`
+);
+
+// 3.
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.recommendedFood > dog.curFood)
+  .flatMap(dog => dog.owners)
+  .join(' and ');
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.recommendedFood < dog.curFood)
+  .flatMap(dog => dog.owners)
+  .join(' and ');
+
+// 4.
+console.log(`${ownersEatTooMuch}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle}'s dogs eat too little!'`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// 6.
+const checkEatingOk = dog =>
+  dog.curFood > dog.recommendedFood * 0.9 &&
+  dog.curFood < dog.recommendedFood * 1.1;
+
+const dogsEatingOk = dogs.some(checkEatingOk);
+console.log(dogsEatingOk);
+
+// 7.
+console.log(dogs.filter(checkEatingOk));
+
+// 8.
+const dogsSorted = dogs
+  .slice()
+  .sort((a, b) => (a.recommendedFood < b.recommendedFood ? 1 : -1));
+
+console.log(dogsSorted);
