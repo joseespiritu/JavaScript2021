b42be93a6c8be8a36509ac27cbe94cf55a17fc6e
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-11-08 17:14:06 -0600

    Timers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -291,14 +291,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -522,19 +524,39 @@ btnSort.addEventListener('click', function (e) {
 
 ///////////////////////////////////
 // Internationalizing dates & numbers (Intl)
+// const num = 388841225.23;
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius',
+//   currency: 'EUR',
+//   // useGrouping: false,
+// };
 
-const num = 388841225.23;
-const options = {
-  style: 'currency',
-  unit: 'celsius',
-  currency: 'EUR',
-  // useGrouping: false,
-};
+// console.log('US =>', new Intl.NumberFormat('en-US', options).format(num));
+// console.log('Germany =>', new Intl.NumberFormat('de-DE', options).format(num));
+// console.log('Syria =>', new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log(
+//   'Browser =>',
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
 
-console.log('US =>', new Intl.NumberFormat('en-US', options).format(num));
-console.log('Germany =>', new Intl.NumberFormat('de-DE', options).format(num));
-console.log('Syria =>', new Intl.NumberFormat('ar-SY', options).format(num));
-console.log(
-  'Browser =>',
-  new Intl.NumberFormat(navigator.language, options).format(num)
+/////////////////////////////////////
+// Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
 );
+
+console.log('Waiting....');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
