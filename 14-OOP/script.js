915a50e93cbaddcd67c35a2fac08e5186d941cac
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-11-23 23:21:44 -0600

    Setters and Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -112,8 +112,8 @@
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -125,11 +125,27 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -141,3 +157,25 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Class are first-class citizes
 // 3. CLasses are executed in strict mode
+
+const walter = new PersonCl('Walter white', 1965);
+
+/////////////////////////////
+// Getters & Setters
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
