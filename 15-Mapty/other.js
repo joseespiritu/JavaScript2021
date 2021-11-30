3a9a4b9f0f1bf8c762d9758186a47bd98bf6b869
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-11-29 23:00:23 -0600

    Displaying a map Leaftlet

---
+++ 15-Mapty/other.js
@@ -0,0 +1,2 @@
+const firstName = 'Jose';
+// console.log(months);
--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -17,6 +17,23 @@ if (navigator.geolocation)
       //   console.log(position);
       const { latitude, longitude } = position.coords;
       console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com.mx/maps/@${latitude},${longitude},15z`
+      );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
