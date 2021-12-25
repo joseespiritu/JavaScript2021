1f909f44881bf38677bc6334780dc16ad093e470
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-12-24 19:47:36 -0600

    Introducction to NPM

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -81,3 +81,20 @@ console.log(cart);
 
 // // Import
 // const { addToCart } = require('./shoppingCart.js');
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+// const stateClone = Object.assign({}, state);
+// console.log(stateClone);
+// state.user.loggedIn = false;
+
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone);
