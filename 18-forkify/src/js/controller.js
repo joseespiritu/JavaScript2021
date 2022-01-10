efda8fb8ecfd032ca39d57e9642bb337c8641885
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-01-09 22:41:42 -0600

    Implementing Error and success messages

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -24,7 +24,9 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    // console.log(err);
+    // recipeView.renderError(`${err} 🔥🔥🔥🔥`);
+    recipeView.renderError();
   }
 };
 
