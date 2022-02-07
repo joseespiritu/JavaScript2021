b785859f1028647229f003bcdd5cc4fa13bd7f3d
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-02-06 20:30:36 -0600

    Fix pagination update, bookmarks on recipe

--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -10,6 +10,7 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -27,10 +28,15 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
+
     console.log(state.recipe);
   } catch (err) {
     // Temp error handling
-    // console.error(`${err} 🔥🔥🔥🔥`);
+    console.error(`${err} 🔥🔥🔥🔥`);
     throw err;
   }
 };
@@ -50,6 +56,8 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+
+    state.search.page = 1;
 
     // console.log(state.search.results);
   } catch (err) {
@@ -74,4 +82,21 @@ export const updateServings = function (newServings) {
   });
 
   state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
